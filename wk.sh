855aa2dd53ad6f2ed300371efa713e13b61fbd3a
wangyu <wangyu@wangyus-iMac.local>
2017-12-20 13:35:48 +0800

    first2

--- wk.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/bash
-
-sudo apt-get update
-sudo apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev cmake
-wget https://github.com/wangyu1997/xmr.git
-cd xmr&&cmake .&&make install
-cp config.txt bin/
-cd bin&&nano config.txt
-
